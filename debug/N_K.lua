1cd325bf6a351a9a0e37df1c5b617a097bf26716
MARTER4345 <86088185+MARTER4345@users.noreply.github.com>
2025-02-04 18:10:58 +1100

    Update N_K.lua

--- debug/N_K.lua
+++ debug/N_K.lua
@@ -1,15 +1,27 @@
+-- G_T__Y --
+local COREGUI = game:GetService("CoreGui")
+if not game:IsLoaded() then
+    local notLoaded = Instance.new("Message")
+    notLoaded.Parent = COREGUI
+    notLoaded.Text = "Waiting for the game to load..."
+    game.Loaded:Wait()
+    notLoaded:Destroy()
+end
+
 local CoinFolder = game.workspace:WaitForChild("Coins")
 local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/MARTER4345/bs2/main/library.lua"))()
 local api = loadstring(game:HttpGet("https://raw.githubusercontent.com/MARTER4345/bs2/main/api.lua"))()
 local player = game.Players.LocalPlayer
+local temptabledead = false
 
-function GetClosestPart(path)
+-- # FUNCS
+function GetClosestPart(folder)
     local Closest
     local root = player.Character and player.Character:FindFirstChild("HumanoidRootPart")
     if not root then return nil end
 
-    for _, v in pairs(path:GetDescendants()) do
-        if v and (v:IsA("BasePart") or v:IsA("Part") or v:IsA("MeshPart")) then
+    for _, v in pairs(folder:GetDescendants()) do
+        if v and v:IsA("BasePart") then
             if not Closest or (root.Position - v.Position).magnitude < (root.Position - Closest.Position).magnitude then
                 Closest = v
             end
@@ -18,13 +30,14 @@ function GetClosestPart(path)
     return Closest
 end
 
-function GetClosestPartNew(table)
+
+function GetClosestPartNew(list)
     local Closest
     local root = player.Character and player.Character:FindFirstChild("HumanoidRootPart")
     if not root then return nil end
 
-    for _, v in pairs(table) do
-        if v and (v:IsA("BasePart") or v:IsA("Part") or v:IsA("MeshPart")) then
+    for _, v in pairs(list) do
+        if v and v:IsA("BasePart") then
             if not Closest or (root.Position - v.Position).magnitude < (root.Position - Closest.Position).magnitude then
                 Closest = v
             end
@@ -33,12 +46,12 @@ function GetClosestPartNew(table)
     return Closest
 end
 
-function GetClosestPartFromPart(table, part)
+function GetClosestPartFromPart(list, part)
     if not part then return nil end
     local Closest
 
-    for _, v in pairs(table) do
-        if v and (v:IsA("MeshPart") or v:IsA("Part")) then
+    for _, v in pairs(list) do
+        if v and v:IsA("BasePart") then
             if not Closest or (part.Position - v.Position).magnitude < (part.Position - Closest.Position).magnitude then
                 Closest = v
             end
@@ -49,26 +62,28 @@ end
 
 function DecancerCoins()
     local CoinParts = {}
-    for _, v in ipairs(CoinFolder:GetDescendants()) do
-        if v and (v:IsA("BasePart") or v:IsA("Part") or v:IsA("MeshPart")) then
-            if v:FindFirstChild("TouchInterest") then
-                if v.Parent and v.Parent.Name == "ROUND SIX" then
-                    v.Name = api.generaterandomstring(12)
+    
+    pcall(function()
+        for _, v in ipairs(CoinFolder:GetDescendants()) do
+            if v and v:IsA("BasePart") then
+                if v:FindFirstChild("TouchInterest") then
+                    if v.Parent and v.Parent.Name == "ROUND SIX" then
+                        v.Name = api.generaterandomstring(12)
+                    end
+                    table.insert(CoinParts, v)
                 end
-                table.insert(CoinParts, v)
             end
         end
-    end
+    end)
+    
     return CoinParts
 end
 
 function Sort()
     local SortedCoins = {}
     local TableToSort = DecancerCoins()
-    
+
     if #TableToSort > 0 then
-        --print("Init : " .. tostring(#TableToSort) .. " c")
-        
         local SCoin = GetClosestPartNew(TableToSort)
         if SCoin then
             table.insert(SortedCoins, SCoin)
@@ -80,7 +95,7 @@ function Sort()
 
         while #TableToSort > 0 do
             task.wait()
-            
+
             local PrevCoin = SCoin
             local Scoin = GetClosestPartFromPart(TableToSort, PrevCoin)
 
@@ -91,23 +106,23 @@ function Sort()
                 if removeIndex then
                     table.remove(TableToSort, removeIndex)
                 end
-                
+
                 SCoin = Scoin
             end
         end
     end
-    
-    print(#SortedCoins.." C$")
+
     return SortedCoins
 end
 
 function GetCoins()
     local playerpart = player.Character and player.Character:FindFirstChild("HumanoidRootPart")
-    if not playerpart then return end
+    if not playerpart or temptabledead then return end
 
     local YummerCoins = Sort()
     for _, v in ipairs(YummerCoins) do
-        if v and v:FindFirstChild("TouchInterest") then 
+        task.wait()
+        if v and v:FindFirstChild("TouchInterest") then
             firetouchinterest(playerpart, v, 0)
             task.wait()
             firetouchinterest(playerpart, v, 1)
@@ -119,13 +134,54 @@ end
 player.CharacterAdded:Connect(function(char)
     local humanoid = char:WaitForChild("Humanoid", 10)
     local playerpart = char:WaitForChild("HumanoidRootPart", 10)
+    
     if humanoid and playerpart then
         humanoid.Died:Connect(function()
-            print("PDeD")
+            temptabledead = true
+            print("PDeD.")
+            task.wait(10)
+            temptabledead = false
         end)
     end
 end)
 
+game:GetService("Players").LocalPlayer.PlayerGui.GameGui.BottomRightContainer.SkipFrame.NameHolder.ChildAdded:Connect(function(child)
+    if child and child.Name == api.lplrid() then
+        task.wait(10)
+        game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("SkipScene"):FireServer()
+    end
+end)
+
+
+task.spawn(function()
+    task.wait(10)
+    local skipFrame = game:GetService("Players").LocalPlayer.PlayerGui.GameGui.BottomRightContainer.SkipFrame.NameHolder
+    if skipFrame:FindFirstChild("150148708") then
+        game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("SkipScene"):FireServer()
+    end
+    
+    task.wait(40)
+    game:GetService("ReplicatedStorage"):WaitForChild("VIPCommands"):WaitForChild("SetMap"):InvokeServer("Chapter 4")
+    
+    task.wait(3)
+    game:GetService("ReplicatedStorage"):WaitForChild("VIPCommands"):WaitForChild("SetMode"):InvokeServer("Bot")
+end)
+
 while task.wait(5) do
-    pcall(GetCoins)
+    if not temptabledead then
+        local coinAmount = game:GetService("Players").LocalPlayer.PlayerGui.GameGui.BottomRightContainer.HudFrame.Coins.Amount
+        if coinAmount and coinAmount.Text == "MAX" then
+            local humanoid = player.Character and player.Character:FindFirstChildOfClass("Humanoid")
+            if humanoid then
+                humanoid:ChangeState(Enum.HumanoidStateType.Dead)
+            end
+        else
+            pcall(function()
+                if temptabledead ~= true and api.humanoidrootpart() then
+                    api.teleport(CFrame.new(-116.023346, -112.200012, 123.903557))
+                end
+            end)
+            pcall(GetCoins)
+        end
+    end
 end
